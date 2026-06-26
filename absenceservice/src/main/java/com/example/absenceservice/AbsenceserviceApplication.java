6338ffc29d24ad83d554c958b04fce56810309cf
sgonalco <sgonalco@emeal.nttdata.com>
2026-06-26 14:11:55 +0200

    configuración de AbsenceService

---
+++ absenceservice/src/main/java/com/example/absenceservice/AbsenceserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.absenceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsenceserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsenceserviceApplication.class, args);
+	}
+
+}
