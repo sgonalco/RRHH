d009f6d7081c10e7002dae2e19e1de4338a08a71
sgonalco <sgonalco@emeal.nttdata.com>
2026-06-26 14:05:02 +0200

    configuración de PayrollService

---
+++ payrollservice/src/main/java/com/example/payrollservice/PayrollserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.payrollservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollserviceApplication.class, args);
+	}
+
+}
