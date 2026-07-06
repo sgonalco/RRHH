dc9dc52643cdc3ba8062323a2b76e5089f7950d2
sgonalco <sgonalco@emeal.nttdata.com>
2026-07-06 10:54:53 +0200

    creacion de modelos base dapartment + project

---
+++ src/main/java/com/example/rrhh/model/Department.java
@@ -0,0 +1,91 @@
+package com.example.rrhh.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "DEPARTMENT")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @NotBlank
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotNull // cambiar a relacion (clave foranea)
+    @Column(name = "manager_id", nullable = false)
+    private Integer managerId;
+
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    @OneToMany(
+            mappedBy = "departmentId",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private Set<Project> projects = new HashSet<>();
+
+    // agregar la relacion manytomany con empleados
+
+    public Department() {}
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(Integer managerId) {
+        this.managerId = managerId;
+    }
+
+    public Set<Project> getGoals() {
+        return projects;
+    }
+
+    public void setGoals(Set<Project> projects) {
+        this.projects = projects;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Set<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(Set<Project> projects) {
+        this.projects = projects;
+    }
+}
---
+++ src/main/java/com/example/rrhh/model/Project.java
@@ -0,0 +1,97 @@
+package com.example.rrhh.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Table(name = "PROJECT")
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @NotBlank
+    @Column(name = "title")
+    private String title;
+
+    @NotBlank
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department departmentId;
+
+    public Project() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public Department getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Department departmentId) {
+        this.departmentId = departmentId;
+    }
+}
